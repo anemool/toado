96346efa4c297840ddf63cc1dc52e124710e956b
Ahmed Ben Moussa <ahmed.benmoussa2@cgm.com>
2024-11-01 19:14:24 +0100

    initial, added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onLogin() {
+    if (this.username === 'a' && this.password === 'a') {
+      alert('Login successful!');
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+}
---
+++ src/app/model/user.model.ts
@@ -0,0 +1,5 @@
+export class User {
+  private userName: string = '';
+  private password: string = '';
+  private email: string = '';
+}
