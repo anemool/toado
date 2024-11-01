809e0e608344d5143b5725244aff332e1366c34c
Ahmed Ben Moussa <ahmed.benmoussa2@cgm.com>
2024-11-01 19:32:25 +0100

    added Header and started working on adding tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
